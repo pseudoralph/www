a0df887d2de0439217e31eaafa01cde01b8025dd
Ralph Perdomo <ralph@prdmo.com>
2019-05-09 11:58:39 -0700

    save

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `prdmo`,
-    description: `musings from a writer turned front-end dev`,
-    author: `Ralph Perdomo`
+    title: 'prdmo',
+    description: 'front-end web dev',
+    author: 'Ralph Perdomo'
   },
   plugins: [
     'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        mergeScriptHashes: true, // you can disable scripts sha256 hashes
-        mergeStyleHashes: true, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
-        directives: {
-          'script-src': "'self'",
-          'style-src': "'self'",
-          'img-src': "'self'"
-          // you can add your directives or override defaults
-        }
-      }
-    },
+    // {
+    //   resolve: `gatsby-plugin-csp`,
+    //   options: {
+    //     disableOnDev: true,
+    //     mergeScriptHashes: true, // you can disable scripts sha256 hashes
+    //     mergeStyleHashes: true, // you can disable styles sha256 hashes
+    //     mergeDefaultDirectives: true,
+    //     directives: {
+    //       'script-src': "'self'",
+    //       'style-src': "'self'",
+    //       'img-src': "'self'"
+    //       // you can add your directives or override defaults
+    //     }
+    //   }
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
