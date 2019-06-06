68b7fc732db5b80f7917265ed7c5ab4edb76d964
Ralph Perdomo <ralph@prdmo.com>
2019-06-05 22:11:22 -0700

    save

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,40 +2,36 @@ const path = require('path');
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
+  const postTemplate = path.resolve(`src/templates/post.js`);
 
-  return new Promise((resolve, reject) => {
-    const postTemplate = path.resolve(`src/templates/post.js`);
-
-    resolve(
-      graphql(`
-        query {
-          allMarkdownRemark {
-            edges {
-              node {
-                frontmatter {
-                  title
-                  path
-                  date
-                  excerpt
-                }
-                id
-              }
+  return graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              path
+              date
+              excerpt
             }
+            id
           }
         }
-      `)
-    ).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        const path = node.frontMatter.path;
-        createPage({
-          path,
-          component: postTemplate,
-          context: {
-            pathSlug: path,
-            render: true
-          }
-        });
-        resolve();
+      }
+    }
+  `).then(result => {
+    console.log(result);
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      // const path = node.frontMatter.path;
+      console.log(node);
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+        context: {
+          pathSlug: path,
+          render: true
+        }
       });
     });
   });
