1d40f360a9e94b1ad71428133d6f11c158caf4e8
Ralph Perdomo <ralph@prdmo.com>
2019-06-06 12:30:09 -0700

    update post template formatting

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-const Post = props => {
-  console.log(props);
-
-  return <div>hello, world</div>;
+const Post = ({
+  data: {
+    markdownRemark: { html }
+  }
+}) => {
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
 };
 
 export default Post;
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+    }
+  }
+`;
