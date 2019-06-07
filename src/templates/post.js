431941f77105ff58558efd0aa3c3ad16df6fa5b9
Ralph Perdomo <ralph@prdmo.com>
2019-06-07 16:39:57 -0700

    update styling and add new entry elements

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import menuHOC from '../components/menuHOC';
+import moment from 'moment';
 
 const Post = ({
   data: {
-    markdownRemark: { html }
+    markdownRemark: {
+      html,
+      frontmatter: { title, date, tags }
+    }
   }
 }) => {
+  const prettyDate = moment(`${date}`).format('dddd, MMMM Do YYYY');
   return (
     <div>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div className="blog-post-div">
+        <div>
+          <h1>{title}</h1>
+          <p>{prettyDate}</p>
+          <p>{tags}</p>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
     </div>
   );
 };
 
-export default Post;
+export default menuHOC(Post);
+// (filter: {frontmatter: {category: {eq: "words"}}})
 
 export const query = graphql`
   query($pathSlug: String!) {
     markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      frontmatter {
+        title
+        date
+        tags
+        category
+      }
       html
     }
   }
