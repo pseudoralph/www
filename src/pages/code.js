033d645a59c6d55da42d5163d2c140d663cb50ba
Ralph Perdomo <ralph@prdmo.com>
2019-06-16 15:24:32 -0700

    update styling for code section

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,74 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
 import menuHOC from '../components/menuHOC';
 import '../style/layout.css';
 import '../style/content.css';
+import { code, web } from '../images/ui';
+import { domino, vignette, volunteer } from '../images/icons';
 
-const styling = {
-  expand: {
-    fontFamily: 'monospace',
-    display: 'inline-block',
-    position: 'absolute',
-    padding: '.1em .8em',
-    margin: '0',
-    right: '0',
-    bottom: '0',
-    backgroundColor: 'black',
-    color: 'white',
-    fontWeight: '700',
-    cursor: 'pointer'
+const codingProjectsDB = [
+  {
+    title: 'Double 9 dominos',
+    description: `A multi-player double nine's (Cuban variant) game of dominoes.  Manages data, control, and session state through redux.`,
+    site: 'https://domino.prdmo.com/#/',
+    git: 'https://github.com/pseudoralph/react-domino',
+    icon: domino,
+    alt: 'domino tile ficha',
+    techs: ['React', 'Redux', 'Firebase']
   },
-
-  ul: { display: 'inline-block', margin: '0', padding: '0' },
-  li: {
-    display: 'inline-block',
-    padding: '.2em',
-    border: '.1em solid black',
-    margin: '.4em'
+  {
+    title: 'Vignette',
+    description: `A microblogging site crossed with Snapchat. A proof-of-concept that shows how ephemerality can exist in an always-on and continuously archived digital world.`,
+    site: 'http://vignette.prdmo.com',
+    git: 'https://github.com/pseudoralph/vignette',
+    icon: vignette,
+    alt: 'vignette photo effect',
+    techs: ['React']
+  },
+  {
+    title: 'Volunteer Tracker',
+    description: `A mobile web application I created to simplify the management and organization of teams working on projects. This app uses CRUD principles through postgreSQL.`,
+    site: 'http://tracker.prdmo.com/',
+    git: 'https://github.com/pseudoralph/volunteer_tracker',
+    icon: volunteer,
+    alt: 'hand raised',
+    techs: ['Ruby', 'pSQL']
   }
-};
+];
 
 function Code() {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const expandedDiv = React.createRef();
   return (
     <div className="outer-wrapper">
       <div className="title-heading-div">
         <h1 id="title-heading">/code</h1>
       </div>
       <div className="articles-wrapper">
-        <div
-          style={{
-            width: '50%',
-            border: '.2em solid black',
-            borderRadius: '.2em',
-            padding: '.7em',
-            position: 'absolute'
-          }}
-        >
-          <p>Double 9 dominos</p>
-          <div
-            style={styling.expand}
-            onClick={() => {
-              setIsExpanded(!isExpanded);
-              expandedDiv.current.style.display =
-                expandedDiv.current.style.display === 'none' ? 'block' : 'none';
-            }}
-          >
-            {isExpanded ? 'collapse [-]' : 'expand [+]'}
+        {codingProjectsDB.map(project => (
+          <div className="code-box" key={project.title}>
+            <img
+              className="project-image"
+              src={project.icon}
+              alt={project.alt}
+            />
+            <h2>{project.title}</h2>
+
+            <div className="code-description">
+              <p>{project.description}</p>
+              <div>
+                <a
+                  href={project.site}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="goto-links">
+                    <img src={web} alt="" />
+                    Visit app
+                  </div>
+                </a>
+                <a href={project.git} target="_blank" rel="noopener noreferrer">
+                  <div className="goto-links">
+                    <img src={code} alt="" />
+                    View code
+                  </div>
+                </a>
+              </div>
+
+              <div className="code-tags">
+                <p>
+                  {project.techs.length > 1
+                    ? 'Technolgies used:'
+                    : 'Technolgy used:'}
+                </p>
+                <ul>
+                  {project.techs.map(tech => (
+                    <li key={tech}>{tech}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
-          <div ref={expandedDiv} style={{ display: 'none' }}>
-            <p>
-              A multi-player double nine's (Cuban variant) game of dominoes.
-              Manages data, control, and session state through redux. Uses
-              Firebase back end with a React front end.
-            </p>
-            <ul style={styling.ul}>
-              <li style={styling.li}>React</li>
-              <li style={styling.li}>Redux </li>
-              <li style={styling.li}>Firebase</li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
