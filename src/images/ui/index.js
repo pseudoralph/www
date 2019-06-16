00686c6709363e1611db24bbdafc6d4d2fabba76
Ralph Perdomo <ralph@prdmo.com>
2019-06-16 15:24:13 -0700

    add new resources to ui module

--- src/images/ui/index.js
+++ src/images/ui/index.js
@@ -2,5 +2,7 @@ import menuBarIcon from './menu-bar-ico.svg';
 import closeButtonIcon from './close-ico.svg';
 import linkedin from './linkedin-logo.svg';
 import github from './github-logo.svg';
+import code from './code.svg';
+import web from './web.svg';
 
-export { menuBarIcon, closeButtonIcon, linkedin, github };
+export { menuBarIcon, closeButtonIcon, linkedin, github, code, web };
